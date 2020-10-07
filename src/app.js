4b019e8407d09644658f2583b65ad3eb8f913fa3
Chris McArdle <christopher.mcardle2@gmail.com>
2020-10-07 15:30:29 -0500

    deploy

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const fRouter = require('./folders/folders-router.js');
 
 const app = express();
 
- const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+ const morganSetting = NODE_ENV === 'production' ? 'tiny' : 'common'
  app.use(morgan(morganSetting))
 app.use(helmet());
 app.use(cors());
