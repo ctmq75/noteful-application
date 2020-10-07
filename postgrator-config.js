30f0347cd7b16988f4a0fec596fc7180244f7de2
Chris McArdle <christopher.mcardle2@gmail.com>
2020-10-07 14:46:58 -0500

    deploy

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,18 @@
   
 require('dotenv').config();
 
+module.exports = {
+  'migrationsDirectory': 'migrations',
+  'driver': 'pg',
+  'connectionString': (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
+  'ssl': !!process.env.SSL,
+  validateChecksums: false,
+};
 
 
-
-
+/*
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
@@ -15,3 +23,4 @@ module.exports = {
   "password": process.env.PROD_MIGRATION_DB_PASS,
   "ssl": true
 }
+*/
--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 app.use((error, req, res, next) => {
   let response
   if (process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'server error' }}
+    response = { error: { message: 'server error edited' }}
   } else {
     response = { error }
   }
